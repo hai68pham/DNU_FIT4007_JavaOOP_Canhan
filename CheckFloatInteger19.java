49f5662dbdacf78b8d2a5fec4d8a10b93fa49c2d
hai68pham <phhai682006mt@gmail.com>
2025-10-23 22:19:14 +0700

    Add files via upload

---
+++ CheckFloatInteger19.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CheckFloatInteger19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập một số thực: ");
+        float a = sc.nextFloat();
+
+        if (a == (int)a) {
+            System.out.println(a + " là số nguyên.");
+        } else {
+            System.out.println(a + " không phải là số nguyên.");
+        }
+
+        sc.close();
+    }
+}
